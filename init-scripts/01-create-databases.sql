a6e573cf3f182ed4101ddc3a922dd42929fb8aee
kempedron@gmail.com <kepr@archlinux.localdomain>
2026-03-13 15:58:24 +0300

    to push

--- init-scripts/01-create-databases.sql
+++
@@ -1,9 +0,0 @@
--- -- Создаем базу данных для auth-service
--- CREATE DATABASE auth_db;
-
--- -- Создаем базу данных для article-service
--- CREATE DATABASE article_db;
-
--- -- Предоставляем права пользователю dbUser
--- GRANT ALL PRIVILEGES ON DATABASE auth_db TO dbUser;
--- GRANT ALL PRIVILEGES ON DATABASE article_db TO dbUser;
